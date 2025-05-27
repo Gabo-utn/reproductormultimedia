fe5cbbd2e6e6582956a8284965d33cc34fcbb761
DaroNelson <churydaro@gmail.com>
2025-05-27 09:11:16 -0300

    lista de reproductor

---
+++ reprod.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+
+#color 
+co1 = "#ffffff" #blanco
+co2 = "#3C1DC6" #violeta//purple 
+co3 = "#333333" #black 
+co4 = "#CFC7F8" #violeta claro// light purple
+
+#ventana de reproduc definida 
+
+window = Tk()
+window.title("Reproductor Multimedia")
+window.geometry('554x256')
+window.configure(background=co1)
+window.resizable(width=FALSE, height=FALSE)
+window.mainloop()
+
+
+
